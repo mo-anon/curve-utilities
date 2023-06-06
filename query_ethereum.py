d08e9bfe7d162444098643d6304384564956398f
mo <isoup@gmx.at>
2023-06-06 16:33:37 +0200

    fix: ty benny

--- query_ethereum.py
+++ query_ethereum.py
@@ -60,79 +60,18 @@ def main(api_url):
         # get admin_balances of the pool. there must be an easier way to do this????
         totalUSD = 0
         admin_balances = metaregistry.get_admin_balances(poolADDR)
+        admin_balances = admin_balances[:len(tokens)]
+        symbols = [token['symbol'] for token in tokens]
+        usdPrice = [token['usdPrice'] for token in tokens] or 0
+        decimals = [token['decimals'] for token in tokens]
 
-        if len(tokens) == 2:
-            s0 = tokens[0]['symbol']
-            s1 = tokens[1]['symbol']
-            p1 = tokens[1]['usdPrice'] or 0
-            p0 = tokens[0]['usdPrice'] or 0
-            d0 = float(tokens[0]['decimals'])
-            d1 = float(tokens[1]['decimals'])
-            balance0 = admin_balances[0] / 10**d0
-            balance1 = admin_balances[1] / 10**d1
-            balance0usd = balance0 * p0
-            balance1usd = balance1 * p1
-            print(s0 + ": " + str(balance0))
-            print(s1 + ": " + str(balance1))
-            totaUnclaimedUSD = balance0usd + balance1usd
-            totalUSD += totaUnclaimedUSD
-            grandTOTAL += totaUnclaimedUSD
+        for (a, b, c, d) in zip(admin_balances, symbols, usdPrice, decimals):
+            tokenAmount = a / 10**float(d)
+            usdVaule = tokenAmount * c
+            totalUSD += usdVaule
+            print(b + ": " + str(usdVaule))
 
-
-        elif len(tokens) == 3:
-            s0 = tokens[0]['symbol']
-            s1 = tokens[1]['symbol']
-            s2 = tokens[2]['symbol']
-            p0 = tokens[0]['usdPrice'] or 0
-            p1 = tokens[1]['usdPrice'] or 0
-            p2 = tokens[2]['usdPrice'] or 0
-            d0 = float(tokens[0]['decimals'])
-            d1 = float(tokens[1]['decimals'])
-            d2 = float(tokens[2]['decimals'])
-            balance0 = admin_balances[0] / 10**d0
-            balance1 = admin_balances[1] / 10**d1
-            balance2 = admin_balances[2] / 10**d2
-            balance0usd = balance0 * p0
-            balance1usd = balance1 * p1
-            balance2usd = balance2 * p2
-            print(s0 + ": " + str(balance0))
-            print(s1 + ": " + str(balance1))
-            print(s2 + ": " + str(balance2))
-            totaUnclaimedUSD = balance0usd + balance1usd + balance2usd
-            totalUSD += totaUnclaimedUSD
-            grandTOTAL += totaUnclaimedUSD
-                    
-        elif len(tokens) == 4:
-            s0 = tokens[0]['symbol']
-            s1 = tokens[1]['symbol']
-            s2 = tokens[2]['symbol']
-            s3 = tokens[3]['symbol']
-            p0 = tokens[0]['usdPrice'] or 0
-            p1 = tokens[1]['usdPrice'] or 0
-            p2 = tokens[2]['usdPrice'] or 0
-            p3 = tokens[3]['usdPrice'] or 0
-            d0 = float(tokens[0]['decimals'])
-            d1 = float(tokens[1]['decimals'])
-            d2 = float(tokens[2]['decimals'])
-            d3 = float(tokens[3]['decimals'])
-            balance0 = admin_balances[0] / 10**d0
-            balance1 = admin_balances[1] / 10**d1
-            balance2 = admin_balances[2] / 10**d2
-            balance3 = admin_balances[3] / 10**d3
-            balance0usd = balance0 * p0
-            balance1usd = balance1 * p1
-            balance2usd = balance2 * p2
-            balance3usd = balance3 * p3
-            print(s0 + ": " + str(balance0))
-            print(s1 + ": " + str(balance1))
-            print(s2 + ": " + str(balance2))
-            print(s3 + ": " + str(balance3))
-            totaUnclaimedUSD = balance0usd + balance1usd + balance2usd + balance3usd
-            totalUSD += totaUnclaimedUSD
-            grandTOTAL += totaUnclaimedUSD
-        else:
-            print("error. pls check")
-
+        grandTOTAL += totalUSD
         print("total unclaimed fees (usd): " + str(totalUSD))
 
         print("")
